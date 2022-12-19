8fd6aa35282273049f6f34f816d9106fb1b8590c
Syahir Zakwan <syahirzakwan.dev@gmail.com>
2022-12-20 02:37:29 +0800

    Create controller Treatment CRUD

---
+++ dentalcare-appointment-system/src/java/com/dentalcare/controller/TreatmentUpdate.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.dentalcare.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author syahir
+ */
+public class TreatmentUpdate extends HttpServlet {
+
+    private PreparedStatement pstmt;
+    
+    @Override
+    public void init() throws ServletException {
+        initializeJdbc();
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        List errorMsgs = new LinkedList();
+        
+        String title = request.getParameter("title");
+        String desc = request.getParameter("desc");
+        String id_param = request.getParameter("id");
+        int id = Integer.parseInt(id_param);
+        
+        try {
+            if(title.isEmpty()) {
+                errorMsgs.add("Title is required");
+            }
+            
+            if(desc.isEmpty()) {
+                errorMsgs.add("Description is required");
+            }
+            
+            if(!errorMsgs.isEmpty()) {
+                request.setAttribute("errorMsgs", errorMsgs);
+                RequestDispatcher view = request.getRequestDispatcher("/admin/treatment/edit.jsp?treat_id="+id);
+                view.forward(request, response);
+                return;
+            }
+            
+            //store data in db
+            updateTreatment(title, desc, id);
+
+            request.setAttribute("successMsgs", "Treatment updated successfully");
+            RequestDispatcher view = request.getRequestDispatcher("/admin/treatment/edit.jsp?treat_id="+id);
+            view.forward(request,response);
+        } catch(IOException | SQLException | ServletException ex) {
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+        //connect with db and query to run
+    private void initializeJdbc() {
+        try {
+            //declare driver and connection string
+            String driver = "org.apache.derby.jdbc.ClientDriver";
+            String connectionString = "jdbc:derby://localhost:1527/DentalcareDB;create=true;user=app;password=app";
+            
+            //load the driver
+            Class.forName(driver);
+            
+            //connect to the database
+            Connection conn = DriverManager.getConnection(connectionString);
+            
+            //store treatment query
+            pstmt = conn.prepareStatement(
+             "UPDATE treatments SET treat_title=?, treat_desc=? WHERE treat_id=?"
+            );
+        } catch (ClassNotFoundException | SQLException ex) {
+        }
+    }
+    
+    //method for store treatment data
+    private void updateTreatment(String title, String desc, int id) throws SQLException {
+        pstmt.setString(1,title);
+        pstmt.setString(2,desc);
+        pstmt.setInt(3,id);
+        pstmt.executeUpdate();
+    }
+}
