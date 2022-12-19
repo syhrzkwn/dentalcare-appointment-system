004c9c7a564c2888bff823c919891542e55cf417
Syahir Zakwan <syahirzakwan.dev@gmail.com>
2022-12-20 02:34:14 +0800

    Edit model Patient and Treatment

--- dentalcare-appointment-system/src/java/com/dentalcare/model/Patient.java
+++ dentalcare-appointment-system/src/java/com/dentalcare/model/Patient.java
@@ -9,27 +9,9 @@ package com.dentalcare.model;
  * @author syahir
  */
 public class Patient extends User {
-    private String role;
     
-    //default constructor
-        public Patient() {
+    //constructor
+    public Patient() {
         super();
-        this.role = "";
-    }
-    
-    //normal constructor
-    public Patient(String firstName, String lastName, String phone, String email, String password, String role) {
-        super(firstName, lastName, phone, email, password);
-        this.role = role;
-    }
-    
-    //mutator
-    public void setRole(String role) {
-        this.role = role;
-    }
-    
-    //accessor
-    public String getRole() {
-        return this.role;
     }
 }
--- dentalcare-appointment-system/src/java/com/dentalcare/model/Treatment.java
+++ dentalcare-appointment-system/src/java/com/dentalcare/model/Treatment.java
@@ -9,5 +9,36 @@ package com.dentalcare.model;
  * @author syahir
  */
 public class Treatment {
+    private int id;
+    private String title;
+    private String desc;
     
+    //constructor
+    public Treatment() {
+        this.id = 0;
+        this.title = "";
+        this.desc = "";
+    }
+    
+    //mutator
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    //accessor
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDesc() {
+        return desc;
+    }
 }
