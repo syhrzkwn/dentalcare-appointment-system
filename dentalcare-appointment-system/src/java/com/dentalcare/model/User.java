182c49e49bafb14bff76213c650bd14836807ddb
Syahir Zakwan <syahirzakwan.dev@gmail.com>
2022-12-20 02:34:48 +0800

    Create model User, Staff, Dentist

---
+++ dentalcare-appointment-system/src/java/com/dentalcare/model/User.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dentalcare.model;
+
+/**
+ *
+ * @author syahir
+ */
+public class User {
+    private int id;
+    private String firstname, lastname, phone, email, status;
+    
+    //constructor
+    public User() {
+        this.id = 0;
+        this.firstname = "";
+        this.lastname = "";
+        this.phone = "";
+        this.email = "";
+        this.status = "";
+    }
+    
+    //mutator
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    //accessor
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstname;
+    }
+    public String getLastName() {
+        return lastname;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getStatus() {
+        return status;
+    }
+}
